4e62e520cf189f432c01235c78c757cda974a3df
Prashanth118 <golla.prashanth118@gmail.com>
2026-04-30 16:22:03 +0530

    Initital commit

---
+++ src/main/java/com/prashanthit/PharmacyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prashanthit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyUserServiceApplication.class, args);
+	}
+
+}
